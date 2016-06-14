ca9886efc38dab097df7cbb3ef37594dc2dfd55e
Kalle Ott <kalle.ott@cap3.de>
2016-06-14 15:32:25 +0200

    first steps to rx websocket server

--- HeightMapStreamServer.cpp
+++ HeightMapStreamServer.cpp
@@ -1,14 +1,71 @@
 #include "HeightMapStreamServer.h"
-#include "rxcpp/rx.hpp"
 
-subject<Kinect::FrameBuffer> HeightMapStreamServer::FrambufferSubject;
+void HeightMapStreamServer::run(uint16_t port) {
+    // listen on specified port
+    m_server.listen(port);
 
-void HeightMapStreamServer::foo() {
-    int i = 0;
-    auto sub = HeightMapStreamServer::FrambufferSubject
-        .get_observable()
-        .subscribe([&](auto frameBuffer){
-            i++;
-            char* frame = frameBuffer.getBuffer();
-        });
+    // Start the server accept loop
+    m_server.start_accept();
+
+    // Start the ASIO io_service run loop
+    try {
+        m_server.run();
+    } catch (const std::exception & e) {
+        std::cout << e.what() << std::endl;
+    }
+}
+
+void HeightMapStreamServer::on_close(connection_hdl hdl) {
+    {
+        lock_guard<mutex> guard(m_action_lock);
+        //std::cout << "on_close" << std::endl;
+        m_actions.push(action(UNSUBSCRIBE,hdl));
+    }
+    m_action_cond.notify_one();
+}
+
+void HeightMapStreamServer::on_message(connection_hdl hdl, server::message_ptr msg) {
+    // queue message up for sending by processing thread
+    {
+        lock_guard<mutex> guard(m_action_lock);
+        //std::cout << "on_message" << std::endl;
+        m_actions.push(action(MESSAGE,hdl,msg));
+    }
+    m_action_cond.notify_one();
+}
+
+void HeightMapStreamServer::process_messages() {
+    while(1) {
+        unique_lock<mutex> lock(m_action_lock);
+
+        while(m_actions.empty()) {
+            m_action_cond.wait(lock);
+        }
+
+        action a = m_actions.front();
+        m_actions.pop();
+
+        lock.unlock();
+
+        if (a.type == SUBSCRIBE) {
+            lock_guard<mutex> guard(m_connection_lock);
+            m_connections.insert(a.hdl);
+        } else if (a.type == UNSUBSCRIBE) {
+            lock_guard<mutex> guard(m_connection_lock);
+            m_connections.erase(a.hdl);
+        } else if (a.type == MESSAGE) {
+            lock_guard<mutex> guard(m_connection_lock);
+
+            con_list::iterator it;
+            for (it = m_connections.begin(); it != m_connections.end(); ++it) {
+                m_server.send(*it,a.msg);
+            }
+        } else {
+            // undefined.
+        }
+    }
+}
+
+void HeightMapStreamServer::addNewFrame(Kinect::FrameBuffer frame) {
+    frameSubject.get_subscriber().on_next(frame);
 }
--- HeightMapStreamServer.h
+++ HeightMapStreamServer.h
@@ -1,17 +1,119 @@
+#define ASIO_STANDALONE
+
+#include "websocketpp/config/asio_no_tls.hpp"
+
+#include "websocketpp/server.hpp"
+
+#include <iostream>
+#include <set>
+
+/*#include <boost/thread.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/condition_variable.hpp>*/
+#include "websocketpp/common/thread.hpp"
+
 #include "rxcpp/rx.hpp"
 #include <Kinect/FrameBuffer.h>
 
-using namespace rxcpp;
-using namespace rxcpp::subjects;
-using namespace rxcpp::operators;
-using namespace rxcpp::util;
+typedef websocketpp::server<websocketpp::config::asio> server;
+
+using websocketpp::connection_hdl;
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;
+using websocketpp::lib::bind;
+
+using websocketpp::lib::thread;
+using websocketpp::lib::mutex;
+using websocketpp::lib::lock_guard;
+using websocketpp::lib::unique_lock;
+using websocketpp::lib::condition_variable;
+
+/* on_open insert connection_hdl into channel
+ * on_close remove connection_hdl from channel
+ * on_message queue send to all channels
+ */
+
+enum action_type {
+    SUBSCRIBE,
+    UNSUBSCRIBE,
+    MESSAGE
+};
+
+struct action {
+    action(action_type t, connection_hdl h) : type(t), hdl(h) {}
+    action(action_type t, connection_hdl h, server::message_ptr m)
+      : type(t), hdl(h), msg(m) {}
+
+    action_type type;
+    websocketpp::connection_hdl hdl;
+    server::message_ptr msg;
+};
+
+typedef websocketpp::server<websocketpp::config::asio> server;
 
 class HeightMapStreamServer {
     public:
-        static subject<Kinect::FrameBuffer> FrambufferSubject;
-        static void foo();
+        HeightMapStreamServer() {
+            // Initialize Asio Transport
+            m_server.init_asio();
+
+            // Register handler callbacks
+            m_server.set_open_handler(bind(&broadcast_server::on_open,this,::_1));
+            m_server.set_close_handler(bind(&broadcast_server::on_close,this,::_1));
+            m_server.set_message_handler(bind(&broadcast_server::on_message,this,::_1,::_2));
+        }
+
+
+        void run(uint16_t port);
+
+        void on_open(connection_hdl hdl);
+
+        void on_close(connection_hdl hdl);
+
+        void on_message(connection_hdl hdl, server::message_ptr msg);
+
+        void process_messages();
+
+        void addNewFrame(Kinect::FrameBuffer frame);
 
     private:
+        typedef std::set<connection_hdl,std::owner_less<connection_hdl> > con_list;
 
+        server m_server;
+        con_list m_connections;
+        std::queue<action> m_actions;
+
+        mutex m_action_lock;
+        mutex m_connection_lock;
+        condition_variable m_action_cond;
+
+        rxcpp::rxsub::subject<FrameBuffer> frameSubject;
 
 };
+
+
+class broadcast_server {
+public:
+    broadcast_server() {
+    }
+
+private:
+
+};
+
+int main() {
+    try {
+        broadcast_server server_instance;
+
+        // Start a thread to run the processing loop
+        thread t(bind(&broadcast_server::process_messages,&server_instance));
+
+        // Run the asio loop with the main thread
+        server_instance.run(9002);
+
+        t.join();
+
+    } catch (websocketpp::exception const & e) {
+        std::cout << e.what() << std::endl;
+    }
+}
