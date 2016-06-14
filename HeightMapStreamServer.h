37ab33e47fddfbc6d2e215a19f3f20e7cd05b89b
Kalle Ott <kalle.ott@cap3.de>
2016-06-14 15:39:52 +0200

    1

--- HeightMapStreamServer.h
+++ HeightMapStreamServer.h
@@ -90,30 +90,3 @@ class HeightMapStreamServer {
         rxcpp::rxsub::subject<FrameBuffer> frameSubject;
 
 };
-
-
-class broadcast_server {
-public:
-    broadcast_server() {
-    }
-
-private:
-
-};
-
-int main() {
-    try {
-        broadcast_server server_instance;
-
-        // Start a thread to run the processing loop
-        thread t(bind(&broadcast_server::process_messages,&server_instance));
-
-        // Run the asio loop with the main thread
-        server_instance.run(9002);
-
-        t.join();
-
-    } catch (websocketpp::exception const & e) {
-        std::cout << e.what() << std::endl;
-    }
-}
